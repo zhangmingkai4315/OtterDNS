a6c98c6d4840221ad1339009976b4c964b937bc0
mike zhang <zhangmingkai.1989@gmail.com>
2020-11-09 17:32:48 +0800

    add dns domain parse

--- src/dns/dns_type.rs
+++ src/dns/dns_type.rs
@@ -1,5 +1,7 @@
 use crate::dns::errors::ParseRRErr;
+use crate::dns::errors::ParseRRErr::ParseTypeErr;
 use crate::dns::utils::*;
+use regex::Regex;
 use std::error::Error;
 use std::net::{Ipv4Addr, Ipv6Addr};
 use std::ops::Deref;
@@ -22,13 +24,14 @@ impl Label {
         if valid_label(s) {
             Ok(Label(Rc::from(s.as_bytes())))
         } else {
-            Err(format!("label is not correct"))
+            Err(format!("{}", s))
         }
     }
 }
 
 #[derive(Debug, PartialOrd, PartialEq)]
 struct DomainName {
+    is_fqdn: bool,
     inner: Vec<Label>,
 }
 
@@ -36,6 +39,93 @@ impl DomainName {
     fn to_string(&self) -> String {
         "".to_owned()
     }
+}
+
+impl Default for DomainName {
+    fn default() -> Self {
+        DomainName {
+            is_fqdn: false,
+            inner: vec![],
+        }
+    }
+}
+impl FromStr for DomainName {
+    type Err = ParseRRErr;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() == 0 || s == "." {
+            // root domain as default
+            return Ok(DomainName::default());
+        }
+        let mut domain = DomainName::default();
+        domain.is_fqdn = is_fqdn(s);
+        for i in s.split('.') {
+            if i.len() == 0 {
+                continue;
+            }
+            match Label::from_str(i) {
+                Ok(v) => domain.inner.push(v),
+                Err(e) => return Err(ParseTypeErr(format!("domain label validate fail: {}", e))),
+            }
+        }
+        Ok(domain)
+    }
+}
+
+#[test]
+fn test_domain_name() {
+    assert_eq!(
+        "hello.google.com".parse::<DomainName>().unwrap(),
+        DomainName {
+            is_fqdn: false,
+            inner: vec![
+                Label::from_str("hello").unwrap(),
+                Label::from_str("google").unwrap(),
+                Label::from_str("com").unwrap()
+            ]
+        }
+    );
+    assert_eq!(
+        "*.google.com".parse::<DomainName>().unwrap(),
+        DomainName {
+            is_fqdn: false,
+            inner: vec![
+                Label::from_str("*").unwrap(),
+                Label::from_str("google").unwrap(),
+                Label::from_str("com").unwrap()
+            ]
+        }
+    );
+    assert_eq!(
+        "_srv.google.com".parse::<DomainName>().unwrap(),
+        DomainName {
+            is_fqdn: false,
+            inner: vec![
+                Label::from_str("_srv").unwrap(),
+                Label::from_str("google").unwrap(),
+                Label::from_str("com").unwrap()
+            ]
+        }
+    );
+    assert_eq!(
+        "google.xn--abc.".parse::<DomainName>().unwrap(),
+        DomainName {
+            is_fqdn: true,
+            inner: vec![
+                Label::from_str("google").unwrap(),
+                Label::from_str("xn--abc").unwrap()
+            ]
+        }
+    );
+
+    assert_eq!(
+        "+hello.google.com".parse::<DomainName>().unwrap_err(),
+        ParseTypeErr("domain label validate fail: +hello".to_owned())
+    );
+    assert_eq!(
+        "hello.&google.com".parse::<DomainName>().unwrap_err(),
+        ParseTypeErr("domain label validate fail: &google".to_owned())
+    );
+    assert_eq!("".parse::<DomainName>().unwrap(), DomainName::default());
 }
 
 #[derive(Debug, PartialOrd, PartialEq)]
@@ -239,3 +329,21 @@ pub struct DnsTypeSOA {
     expire: u32,
     minimum: u32,
 }
+
+//
+// impl FromStr for DnsTypeSOA {
+//     type Err = ParseRRErr;
+//     fn from_str(s: &str) -> Result<Self, Self::Err> {
+//         // localhost. root.localhost. 1999010100 ( 10800 900 604800 86400 )
+//         let s1 = s.replace("(", " ").replace(")", " ");
+//         let mut token = s1.split_whitespace();
+//         let domain: DomainName;
+//         if let Some(v) = token.next(){
+//             match  v.parse::<DomainName>(){
+//                 Ok(dn)=> domain = dn,
+//                 Err(e) => return Err(e),
+//             }
+//         }
+//
+//     }
+// }
