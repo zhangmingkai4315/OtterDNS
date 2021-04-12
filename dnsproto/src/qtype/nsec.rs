8fced09280cc30da1d1e26e88735abe0628a8381
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2021-04-12 10:05:54 +0800

    update nsec test

--- dnsproto/src/qtype/nsec.rs
+++ dnsproto/src/qtype/nsec.rs
@@ -12,8 +12,11 @@
 
 use crate::dnsname::{parse_name, DNSName};
 use crate::meta::DNSType;
+use crate::qtype::soa::is_not_space;
 use crate::qtype::{CompressionType, DNSWireFrame};
 use itertools::enumerate;
+use nom::bytes::complete::take_while;
+use nom::character::complete::multispace0;
 use nom::combinator::rest;
 use otterlib::errors::{DNSProtoErr, ParseZoneDataErr};
 use std::any::Any;
@@ -33,17 +36,22 @@ impl DnsTypeNSEC {
             bitmaps: encode_nsec_from_types(type_arr)?,
         })
     }
-    // NSEC	aaa. NS SOA RRSIG NSEC DNSKEY
-    // pub fn from_str(str: &str, default_original: Option<&str>) -> Result<Self, ParseZoneDataErr> {
-    //     let (rest, priority) = digit1(str)?;
-    //     let priority = u16::from_str(priority)?;
-    //     let (rest, _) = multispace0(rest)?;
-    //     let (_, exchange) = not_space(rest)?;
-    //     Ok(DnsTypeMX {
-    //         priority,
-    //         exchange: DNSName::new(exchange, default_original)?,
-    //     })
-    // }
+    // aaa. NS SOA RRSIG NSEC DNSKEY
+    pub fn from_str(str: &str, default_original: Option<&str>) -> Result<Self, ParseZoneDataErr> {
+        let (rest, _) = multispace0(str)?;
+        let (rest, next_domain) = take_while(is_not_space)(rest)?;
+        let (rest, _) = multispace0(rest)?;
+        let dnstypes = rest
+            .split(" ")
+            .into_iter()
+            .map(|dtype| DNSType::from_str(dtype).unwrap_or(DNSType::Unknown))
+            .collect();
+        let bitmaps = encode_nsec_from_types(dnstypes)?;
+        Ok(DnsTypeNSEC {
+            next_domain: DNSName::new(next_domain, default_original)?,
+            bitmaps,
+        })
+    }
 }
 
 impl fmt::Display for DnsTypeNSEC {
@@ -58,7 +66,8 @@ impl fmt::Display for DnsTypeNSEC {
                             .unwrap_or(DNSType::Unknown)
                             .to_string()
                     })
-                    .collect(),
+                    .collect::<Vec<String>>()
+                    .join(" "),
                 Err(err) => format!("decode fail: {:?}", err),
             }
         };
@@ -113,8 +122,8 @@ fn decode_nsec_from_bits(input: &[u8]) -> Result<Vec<u16>, ParseZoneDataErr> {
     let mut result = Vec::new();
     let message_size = input.len();
     let mut offset = 0;
-    let mut length: usize = 0;
-    let mut window = 0;
+    let mut length: usize;
+    let mut window: u8;
     let mut last_window = -1;
     while offset < message_size {
         if offset + 2 > message_size {
@@ -124,6 +133,7 @@ fn decode_nsec_from_bits(input: &[u8]) -> Result<Vec<u16>, ParseZoneDataErr> {
         }
         window = input[offset];
         length = input[offset + 1] as usize;
+        offset = offset + 2;
         if window as isize <= last_window {
             return Err(ParseZoneDataErr::GeneralErr(
                 "nsec block unpack out of order".to_string(),
@@ -146,16 +156,29 @@ fn decode_nsec_from_bits(input: &[u8]) -> Result<Vec<u16>, ParseZoneDataErr> {
         }
         for (index, block) in enumerate(input[offset..offset + length].iter()) {
             let index = index as u16;
-            match block {
-                block if block & 0x80 == 0x80 => result.push(window as u16 * 256 + index * 8 + 0),
-                block if block & 0x40 == 0x40 => result.push(window as u16 * 256 + index * 8 + 1),
-                block if block & 0x20 == 0x20 => result.push(window as u16 * 256 + index * 8 + 2),
-                block if block & 0x10 == 0x10 => result.push(window as u16 * 256 + index * 8 + 3),
-                block if block & 0x8 == 0x8 => result.push(window as u16 * 256 + index * 8 + 4),
-                block if block & 0x4 == 0x4 => result.push(window as u16 * 256 + index * 8 + 5),
-                block if block & 0x2 == 0x2 => result.push(window as u16 * 256 + index * 8 + 6),
-                block if block & 0x1 == 0x1 => result.push(window as u16 * 256 + index * 8 + 7),
-                _ => {}
+            if block & 0x80 == 0x80 {
+                result.push(window as u16 * 256 + index * 8 + 0)
+            }
+            if block & 0x40 == 0x40 {
+                result.push(window as u16 * 256 + index * 8 + 1)
+            }
+            if block & 0x20 == 0x20 {
+                result.push(window as u16 * 256 + index * 8 + 2)
+            }
+            if block & 0x10 == 0x10 {
+                result.push(window as u16 * 256 + index * 8 + 3)
+            }
+            if block & 0x8 == 0x8 {
+                result.push(window as u16 * 256 + index * 8 + 4)
+            }
+            if block & 0x4 == 0x4 {
+                result.push(window as u16 * 256 + index * 8 + 5)
+            }
+            if block & 0x2 == 0x2 {
+                result.push(window as u16 * 256 + index * 8 + 6)
+            }
+            if block & 0x1 == 0x1 {
+                result.push(window as u16 * 256 + index * 8 + 7)
             }
         }
         offset += length;
@@ -194,9 +217,58 @@ fn encode_nsec_from_types(bitmap: Vec<DNSType>) -> Result<Vec<u8>, ParseZoneData
         last_length = length;
         last_window = window;
     }
-    offset += last_window + 2;
+    // offset += last_window + 2;
     Ok(result)
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use crate::meta::DNSType;
+    use crate::qtype::nsec::DnsTypeNSEC;
+    use crate::qtype::DNSWireFrame;
+
+    fn get_example_nsec() -> (Vec<u8>, String, DnsTypeNSEC) {
+        let nsec_str = "aaa. NS SOA RRSIG NSEC DNSKEY";
+        let nsec_struct = DnsTypeNSEC::new(
+            "aaa.",
+            vec![
+                DNSType::NS,
+                DNSType::SOA,
+                DNSType::RRSIG,
+                DNSType::NSEC,
+                DNSType::DNSKEY,
+            ],
+        );
+        let nsec_bitstream = vec![
+            0x03, 0x61, 0x61, 0x61, 0x00, 0x00, 0x07, 0x22, 0x00, 0x00, 0x00, 0x00, 0x03, 0x80,
+        ];
+        (nsec_bitstream, nsec_str.to_owned(), nsec_struct.unwrap())
+    }
+
+    #[test]
+    fn dns_nsec_from_str() {
+        let (_, nsec_str, nsec_struct) = get_example_nsec();
+        match DnsTypeNSEC::from_str(nsec_str.as_str(), None) {
+            Ok(nsec) => {
+                assert_eq!(nsec, nsec_struct);
+            }
+            Err(err) => assert!(
+                false,
+                format!("nsec from_str method got a unexpected failure: {:?}", err)
+            ),
+        }
+    }
+
+    #[test]
+    fn nsec_binary_serialize() {
+        let (bin_arr, nsec_str, nsec_struct) = get_example_nsec();
+        assert_eq!(DnsTypeNSEC::decode(&bin_arr, None).unwrap(), nsec_struct);
+        match nsec_struct.encode(None) {
+            Ok(nsec_vec) => {
+                assert_eq!(nsec_vec.as_slice(), &bin_arr[..]);
+            }
+            _ => assert!(false, "encode nsec fail"),
+        }
+        assert_eq!(nsec_str, nsec_struct.to_string());
+    }
+}
